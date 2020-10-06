ac03c30a6da87c2c9517ed6f84d5b949d619d824
roy <roycirkin1@gmail.com>
2020-10-06 10:14:24 +0300

    finished

---
+++ src/build/CMakeFiles/ex3.out.dir/DependInfo.cmake
@@ -0,0 +1,48 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/roy/ex3/src/crc/crc32c.c" "/home/roy/ex3/src/build/CMakeFiles/ex3.out.dir/crc/crc32c.c.o"
+  "/home/roy/ex3/src/matrix/ErrorCode.c" "/home/roy/ex3/src/build/CMakeFiles/ex3.out.dir/matrix/ErrorCode.c.o"
+  "/home/roy/ex3/src/matrix/Matrix.c" "/home/roy/ex3/src/build/CMakeFiles/ex3.out.dir/matrix/Matrix.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/roy/ex3/src/BMP/BMPOperation.cpp" "/home/roy/ex3/src/build/CMakeFiles/ex3.out.dir/BMP/BMPOperation.cpp.o"
+  "/home/roy/ex3/src/BMP/BitMap.cpp" "/home/roy/ex3/src/build/CMakeFiles/ex3.out.dir/BMP/BitMap.cpp.o"
+  "/home/roy/ex3/src/BMP/BitMapFactory.cpp" "/home/roy/ex3/src/build/CMakeFiles/ex3.out.dir/BMP/BitMapFactory.cpp.o"
+  "/home/roy/ex3/src/BMP/BitMapHelpFunctions.cpp" "/home/roy/ex3/src/build/CMakeFiles/ex3.out.dir/BMP/BitMapHelpFunctions.cpp.o"
+  "/home/roy/ex3/src/BMP/Exceptions.cpp" "/home/roy/ex3/src/build/CMakeFiles/ex3.out.dir/BMP/Exceptions.cpp.o"
+  "/home/roy/ex3/src/BMP/Headers.cpp" "/home/roy/ex3/src/build/CMakeFiles/ex3.out.dir/BMP/Headers.cpp.o"
+  "/home/roy/ex3/src/BMP/bmp_tester.cpp" "/home/roy/ex3/src/build/CMakeFiles/ex3.out.dir/BMP/bmp_tester.cpp.o"
+  "/home/roy/ex3/src/CacheList.cpp" "/home/roy/ex3/src/build/CMakeFiles/ex3.out.dir/CacheList.cpp.o"
+  "/home/roy/ex3/src/CacheManager.cpp" "/home/roy/ex3/src/build/CMakeFiles/ex3.out.dir/CacheManager.cpp.o"
+  "/home/roy/ex3/src/CacheSearcher.cpp" "/home/roy/ex3/src/build/CMakeFiles/ex3.out.dir/CacheSearcher.cpp.o"
+  "/home/roy/ex3/src/CommandLineParser.cpp" "/home/roy/ex3/src/build/CMakeFiles/ex3.out.dir/CommandLineParser.cpp.o"
+  "/home/roy/ex3/src/FileNode.cpp" "/home/roy/ex3/src/build/CMakeFiles/ex3.out.dir/FileNode.cpp.o"
+  "/home/roy/ex3/src/crc/crcOperation.cpp" "/home/roy/ex3/src/build/CMakeFiles/ex3.out.dir/crc/crcOperation.cpp.o"
+  "/home/roy/ex3/src/main.cpp" "/home/roy/ex3/src/build/CMakeFiles/ex3.out.dir/main.cpp.o"
+  "/home/roy/ex3/src/matrix/ClassMatrix.cpp" "/home/roy/ex3/src/build/CMakeFiles/ex3.out.dir/matrix/ClassMatrix.cpp.o"
+  "/home/roy/ex3/src/matrix/MatrixExceptions.cpp" "/home/roy/ex3/src/build/CMakeFiles/ex3.out.dir/matrix/MatrixExceptions.cpp.o"
+  "/home/roy/ex3/src/matrix/MatrixOperation.cpp" "/home/roy/ex3/src/build/CMakeFiles/ex3.out.dir/matrix/MatrixOperation.cpp.o"
+  "/home/roy/ex3/src/matrix/MatrixParser.cpp" "/home/roy/ex3/src/build/CMakeFiles/ex3.out.dir/matrix/MatrixParser.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/build/CMakeFiles/ex3.out.dir/cmake_clean.cmake
@@ -0,0 +1,30 @@
+file(REMOVE_RECURSE
+  "../bin/ex3.out"
+  "../bin/ex3.out.pdb"
+  "CMakeFiles/ex3.out.dir/BMP/BMPOperation.cpp.o"
+  "CMakeFiles/ex3.out.dir/BMP/BitMap.cpp.o"
+  "CMakeFiles/ex3.out.dir/BMP/BitMapFactory.cpp.o"
+  "CMakeFiles/ex3.out.dir/BMP/BitMapHelpFunctions.cpp.o"
+  "CMakeFiles/ex3.out.dir/BMP/Exceptions.cpp.o"
+  "CMakeFiles/ex3.out.dir/BMP/Headers.cpp.o"
+  "CMakeFiles/ex3.out.dir/BMP/bmp_tester.cpp.o"
+  "CMakeFiles/ex3.out.dir/CacheList.cpp.o"
+  "CMakeFiles/ex3.out.dir/CacheManager.cpp.o"
+  "CMakeFiles/ex3.out.dir/CacheSearcher.cpp.o"
+  "CMakeFiles/ex3.out.dir/CommandLineParser.cpp.o"
+  "CMakeFiles/ex3.out.dir/FileNode.cpp.o"
+  "CMakeFiles/ex3.out.dir/crc/crc32c.c.o"
+  "CMakeFiles/ex3.out.dir/crc/crcOperation.cpp.o"
+  "CMakeFiles/ex3.out.dir/main.cpp.o"
+  "CMakeFiles/ex3.out.dir/matrix/ClassMatrix.cpp.o"
+  "CMakeFiles/ex3.out.dir/matrix/ErrorCode.c.o"
+  "CMakeFiles/ex3.out.dir/matrix/Matrix.c.o"
+  "CMakeFiles/ex3.out.dir/matrix/MatrixExceptions.cpp.o"
+  "CMakeFiles/ex3.out.dir/matrix/MatrixOperation.cpp.o"
+  "CMakeFiles/ex3.out.dir/matrix/MatrixParser.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/ex3.out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
